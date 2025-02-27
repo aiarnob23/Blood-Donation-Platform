4327df63f4864be3592410a8cb92228b7b304ea2
aiarnob23 <aiarnob23@gmail.com>
2025-02-27 11:47:35 +0600

    project initialized

--- frontend/src/firebase/firebase.config.ts
+++ frontend/src/firebase/firebase.config.ts
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBp_ngMvXgvCMjJBZHFr4kmNhqsf0dvSeg",
-  authDomain: "blood-donation-platform-bce54.firebaseapp.com",
-  projectId: "blood-donation-platform-bce54",
-  storageBucket: "blood-donation-platform-bce54.firebasestorage.app",
-  messagingSenderId: "25296583584",
-  appId: "1:25296583584:web:070a9156b1e395b1f802c5",
+  apiKey: ,
+  authDomain: ,
+  projectId: ,
+  storageBucket: ,
+  messagingSenderId: ,
+  appId: ,
 };
 
 // Initialize Firebase
